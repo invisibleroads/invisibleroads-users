4a0ab04b7c901cd0189fa2e760130c18fbf6e9b2
Roy Hyunjin Han <rhh@crosscompute.com>
2015-03-19 21:01:40 -0400

    Restart data folder

--- records/invisibleroads_records/scripts/initialize.py
+++ records/invisibleroads_records/scripts/initialize.py
@@ -1,4 +1,6 @@
+import shutil
 import sys
+from argparse import ArgumentParser
 from invisibleroads_macros import disk
 from os.path import basename
 from pyramid.paster import get_app, setup_logging
@@ -7,13 +9,17 @@ from ..models import Base
 
 
 def main(argv=sys.argv):
-    if len(argv) < 2:
-        print('%s development.ini' % basename(argv[0]))
-        sys.exit(1)
-    configuration_path = argv[1]
-    setup_logging(configuration_path)
-    app = get_app(configuration_path)
+    argument_parser = ArgumentParser()
+    argument_parser.add_argument('configuration_path')
+    argument_parser.add_argument('--restart', action='store_true')
+    args = argument_parser.parse_args()
+
+    setup_logging(args.configuration_path)
+    app = get_app(args.configuration_path)
     settings = app.registry.settings
+
+    if args.restart:
+        shutil.rmtree(settings['data.folder'])
     for key, value in settings.iteritems():
         if key.endswith('.folder'):
             disk.make_folder(value)
