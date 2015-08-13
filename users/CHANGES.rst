ae1401d9b9af09ff68d42fd66a88eafe81f7a836
Roy Hyunjin Han <rhh@crosscompute.com>
2015-08-12 21:17:02 -0400

    Bump version

--- users/CHANGES.rst
+++ users/CHANGES.rst
@@ -1,3 +1,7 @@
+0.2
+---
+- Let downstream packages override User, RootFactory
+
 0.1
 ---
 - Add pyramid_redis_sessions
--- users/setup.py
+++ users/setup.py
@@ -15,7 +15,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst', 'CHANGES.rst'])
 setup(
     name='invisibleroads-users',
-    version='0.1',
+    version='0.2',
     description='Authentication and authorization',
     long_description=DESCRIPTION,
     classifiers=[
