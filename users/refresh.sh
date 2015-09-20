8dd177e9e7eb75344ea92d627bd90e1bb8705454
Roy Hyunjin Han <rhh@crosscompute.com>
2015-09-20 12:28:35 -0400

    Update base.js

---
+++ users/refresh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+MODULE_NAMES="invisibleroads_posts invisibleroads_users"
+for MODULE_NAME in ${MODULE_NAMES}; do
+    PARENT_FOLDER=`python -c "import ${MODULE_NAME}; from os.path import abspath, dirname; print(dirname(dirname(abspath(${MODULE_NAME}.__file__))))"`
+    NODE_FOLDER=${PARENT_FOLDER}/node_modules/${MODULE_NAME/_/-}
+    PYTHON_FOLDER=${PARENT_FOLDER}/${MODULE_NAME}
+    pushd ${NODE_FOLDER} > /dev/null; npm install -g; popd > /dev/null
+    browserify ${NODE_FOLDER}/base.js | uglifyjs -c > ${PYTHON_FOLDER}/assets/base.js
+done
