2873fa6ccc8f626f40a757b7de96584f126da3b1
Roy Hyunjin Han <starsareblueandfaraway@gmail.com>
2015-10-22 11:50:14 -0400

    Move posts

--- invisibleroads/README.rst
+++
@@ -1,3 +0,0 @@
-InvisibleRoads
-==============
-Here is a simple framework for extensible command line scripts.
--- posts/invisibleroads_posts/scaffolds/__init__.py
+++
@@ -1,7 +0,0 @@
-from pyramid.scaffolds import PyramidTemplate
-
-
-class PostsTemplate(PyramidTemplate):
-
-    _template_dir = 'posts'
-    summary = 'InvisibleRoads Posts'
