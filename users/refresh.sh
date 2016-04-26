de83a8cf520167a3f4dd3ff44094c73c70865143
Roy Hyunjin Han <rhh@crosscompute.com>
2016-04-26 09:09:36 -0400

    Shorten lines in refresh.sh

--- users/refresh.sh
+++ users/refresh.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 MODULE_NAMES="invisibleroads_posts invisibleroads_users"
 for MODULE_NAME in ${MODULE_NAMES}; do
-    PARENT_FOLDER=`python -c "import ${MODULE_NAME}; from os.path import abspath, dirname; print(dirname(dirname(abspath(${MODULE_NAME}.__file__))))"`
-    NODE_FOLDER=${PARENT_FOLDER}/node_modules/${MODULE_NAME/_/-}
-    PYTHON_FOLDER=${PARENT_FOLDER}/${MODULE_NAME}
+    ROOT_FOLDER=`python -c "import ${MODULE_NAME}; from os.path import abspath as a, dirname as d; print(d(d(a(${MODULE_NAME}.__file__))))"`
+    NODE_FOLDER=${ROOT_FOLDER}/node_modules/${MODULE_NAME/_/-}
+    PYTHON_FOLDER=${ROOT_FOLDER}/${MODULE_NAME}
     pushd ${NODE_FOLDER} > /dev/null; npm install -g; popd > /dev/null
     if [[ "$1" == "--debug" || "$1" == "-d" ]]; then
         JS=`browserify ${NODE_FOLDER}/library.js`
