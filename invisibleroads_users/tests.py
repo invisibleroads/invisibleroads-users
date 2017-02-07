6577b293b16caae8ba43cb84104222234add22c1
Roy <rhh@crosscompute.com>
2017-02-07 15:51:28 -0500

    Add test constructs

---
+++ invisibleroads_users/tests.py
@@ -0,0 +1,22 @@
+from pytest import fixture
+from redis import StrictRedis
+from webtest import TestApp
+
+from invisibleroads_records import main as get_app
+from invisibleroads_users.models import get_user
+
+
+@fixture
+def users_request(records_request, config, database):
+    config.include('invisibleroads_users')
+    user_id = records_request.authenticated_userid
+    users_request = records_request
+    users_request.authenticated_user = get_user(database, user_id)
+    yield users_request
+    StrictRedis().flushall()
+
+
+@fixture
+def website(settings):
+    yield TestApp(get_app({}, **settings))
+    StrictRedis().flushall()
---
+++ tests/conftest.py
@@ -0,0 +1,5 @@
+pytest_plugins = [
+    'invisibleroads_posts.tests',
+    'invisibleroads_records.tests',
+    'invisibleroads_users.tests',
+]
