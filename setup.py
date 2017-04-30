f4e223faed23485b78787df6f62b96d62d53e4d0
Roy <rhh@crosscompute.com>
2017-04-30 15:14:51 -0400

    Update URL

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     ],
     author='Roy Hyunjin Han',
     author_email='rhh@crosscompute.com',
-    url='http://invisibleroads.com',
+    url='https://github.com/invisibleroads/invisibleroads-users',
     keywords='web wsgi bfg pylons pyramid invisibleroads',
     packages=find_packages(),
     include_package_data=True,
