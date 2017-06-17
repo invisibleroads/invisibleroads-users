089230930f3f5ab5d560785f79c6b161744c8e67
Roy Hyunjin Han <rhh@crosscompute.com>
2017-06-17 00:58:37 -0400

    Use pyramid-multiauth

---
+++ tests/test_users.py
@@ -0,0 +1,9 @@
+from invisibleroads_users.views import see_user
+from pyramid.httpexceptions import HTTPNotFound
+from pytest import raises
+
+
+def test_see_user(users_request):
+    users_request.matchdict['user_id'] = 'x'
+    with raises(HTTPNotFound):
+        see_user(users_request)
